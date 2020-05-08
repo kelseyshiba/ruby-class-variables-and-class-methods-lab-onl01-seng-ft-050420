ac988af9e92ef27abdb63124a74d254c7706908e
Kelsey Shiba <kelsey.shiba@gmail.com>
2020-05-08 16:55:28 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,50 @@
+require 'pry'
+class Song
+  attr_accessor :name, :genre, :artist
+  
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist  
+    @genre = genre
+    @@genres << genre
+    @@count += 1
+    @@artists << artist
+  end
+  
+  @@count = 0
+  
+  def self.count
+    @@count
+  end
+  
+  def self.genres 
+    @@genres.uniq
+  end
+  
+  @@genres = []
+  @@artists = []
+  
+  def self.artists
+    @@artists.uniq
+  end 
+  
+  def self.genre_count
+    #hash[namesgenre] => song_count
+    g_count = {}
+    #@@genres = ["pop", "rock", "pop"]
+    count = 0
+    @@genres.each do |genre|
+          if g_count[genre] 
+            g_count[genre] +=1
+          else
+            g_count[genre] = 1
+      end
+  end
+    g_count
+  end
+  
+  def self.artist_count
+  
+  end
+
+end
